b88c6ab3947df4b2866a7d738bcde6c20f88bff2
Paulo Vitor <paulovitorvaztst@gmail.com>
2025-10-01 11:18:51 -0300

    primeiro commit

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "tailwindcss";
+
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js.jsx,tsx,mdx}",
+    "./src/components/**/*.{js,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
